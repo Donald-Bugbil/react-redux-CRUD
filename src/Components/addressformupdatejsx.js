ae52bb46832cf742107673b1c3a903f680ae8749
Donald Bugbil <donaldmoore340@gmail.com>
2020-11-12 12:00:53 +0000

    update form

--- src/Components/addressformupdatejsx.js
+++ src/Components/addressformupdatejsx.js
@@ -50,31 +50,32 @@ class FormUpdate extends React.Component{
                     <div className="form-row">
                         <div className="form-group col-12">
                             <label htmlFor="countryInput">Country<span style={{ color: 'red' }}>*</span></label>
-                            <input type="text" className="form-control" id="countryInput" name= "countryUpdate" onChange= {this.changeHandler} />
+                            <input type="text" className="form-control" id="countryInput" name= "countryUpdate" onChange= {this.changeHandler} value={this.state.info.countryUpdate}/>
                         </div>
                         <div className="form-group col-6">
                             <label htmlFor="firstName">First name<span style={{ color: 'red' }}>*</span></label>
-                            <input type="text" className="form-control" id="firstName" name="firstNameUpdate" onChange={this.changeHandler} />
+                            <input type="text" className="form-control" id="firstName" name="firstNameUpdate" onChange={this.changeHandler} value={this.state.info.firstNameUpdate
+                            }/>
                         </div>
                         <div className="form-group col-6">
                             <label htmlFor="lastName">Last name<span style={{ color: 'red' }}>*</span></label>
-                            <input type="text" className="form-control" id="lastName" name="lastNameUpdate" onChange={this.changeHandler} />
+                            <input type="text" className="form-control" id="lastName" name="lastNameUpdate" onChange={this.changeHandler} value={this.state.info.lastNameUpdate}/>
                         </div>
                         <div className="form-group col-12">
                             <label htmlFor="companyInput">Company<span style={{ color: 'red' }}>*</span></label>
-                            <input type="text" className="form-control" id="companyInput" name="companyUpdate" onChange={this.changeHandler} />
+                            <input type="text" className="form-control" id="companyInput" name="companyUpdate" onChange={this.changeHandler} value={this.state.info.companyUpdate}/>
                         </div>
                         <div className="form-group col-12">
                             <label htmlFor="streetAddress">Street address<span style={{ color: 'red' }}>*</span></label>
-                            <input type="text" className="form-control" id="streetAddress" name="streetAddressUpdate" onChange={this.changeHandler} />
+                            <input type="text" className="form-control" id="streetAddress" name="streetAddressUpdate" onChange={this.changeHandler} value={this.state.info.streetAddressUpdate}/>
                         </div>
                         <div className="form-group col-3">
                             <label htmlFor="postalCode">Postal code</label>
-                            <input type="text" className="form-control" id="postalCode" name="postalCodeUpdate" onChange={this.changeHandler} />
+                            <input type="text" className="form-control" id="postalCode" name="postalCodeUpdate" onChange={this.changeHandler} value={this.state.info.postalCodeUpdate}/>
                         </div>
                         <div className="form-group col-4">
                             <label htmlFor="cityInput">City<span style={{ color: 'red' }}>*</span></label>
-                            <input type="text" className="form-control" id="cityInput" name="cityUpdate" onChange={this.changeHandler}/>
+                            <input type="text" className="form-control" id="cityInput" name="cityUpdate" onChange={this.changeHandler} value={this.state.info.cityUpdate}/>
                         </div>
                     </div>
                     <button type="submit" className="btn btn-primary">Update</button>
